8855ce99b2cf307931283005ab30099627c52bdc
kenizaya <harveylstark@gmail.com>
2022-09-11 19:32:42 +0530

    Display messages

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+const formatDate = require('date-fns/format')
+var router = express.Router()
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+]
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message Board', messages, formatDate })
+})
+
+module.exports = router
